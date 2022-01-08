dcb22b7b2799674906b9eb5e408f85191399556e
Jaroslaw Warowny <jwarowny99@gmail.com>
2022-01-08 15:25:05 +0100

    Checkers first step

---
+++ src/main/java/com/kodilla/KodillaProject1Application.java
@@ -0,0 +1,13 @@
+package com.kodilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaProject1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaProject1Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/kodilla/Project1/Checkers.java
@@ -0,0 +1,5 @@
+package com.kodilla.Project1;
+
+public class Checkers {
+
+}
